9e8d935005874e667adacdc803d23198c4167876
Zihan Hossain <zihanhossain13@gmail.com>
2026-08-25 22:15:39 +0600

    Initial commit

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -33,33 +35,58 @@ fun ProfileScreen(viewModel:ProfileViewModel=viewModel()) {
         horizontalAlignment=Alignment.CenterHorizontally,
         verticalArrangement=Arrangement.Center
     ){
+
         OutlinedTextField(
             value=viewModel.username.value,
             onValueChange={ new->
-                viewModel.username.value=new
+                viewModel.orNameChange(new)
             }
         )
         Spacer(modifier=Modifier.height(8.dp))
         OutlinedTextField(
             value=viewModel.userAge.value,
             onValueChange={new->
-                viewModel.userAge.value=new
+                viewModel.orAgeChange(new)
             }
         )
         Spacer(modifier=Modifier.height(8.dp))
         Button(onClick={
 viewModel.addUser()
+
         }){
             Text("Add")
         }
         Text("Hello ${viewModel.username.value},you are ${viewModel.userAge.value} years old")
+        LazyColumn(modifier=Modifier.weight(1f)){
+            items(viewModel.list){item->
+                Card(
+                    modifier=Modifier.padding(8.dp).fillMaxWidth(),
+                    elevation=CardDefaults.cardElevation(8.dp)
+                ){
+                    Text(
+                        text=item,
+                        modifier=Modifier.padding(16.dp))
+
+                }
+            }
+        }
     }
+
 }
 class ProfileViewModel:ViewModel(){
     val username=mutableStateOf("")
     val userAge=mutableStateOf("")
-    val list=mutableStateListOf("")
+    val list=mutableStateListOf<String>()
     fun addUser(){
         list.add("${username.value} - ${userAge.value}")
+    username.value=""
+        userAge.value=""
     }
+    fun orNameChange(newName:String){
+        username.value=newName
+    }
+    fun orAgeChange(newAge:String){
+        userAge.value=newAge
+    }
+
 }
